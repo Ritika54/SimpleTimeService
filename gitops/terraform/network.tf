b7d5eb4679dc00af68e212aa79c7c8c2781caee9
Ritika Laddha <69119436+Ritika54@users.noreply.github.com>
2025-12-13 18:07:55 +0530

    changing subnet range, as it's overlapping with default aks cidr range

--- gitops/terraform/network.tf
+++ gitops/terraform/network.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-aks-hybrid"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.1.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
@@ -11,7 +11,7 @@ resource "azurerm_subnet" "public_subnets" {
   name                 = "subnet-public-${count.index + 1}"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.${count.index + 1}.0/24"]
+  address_prefixes     = ["10.1.${count.index + 1}.0/24"]
 }
 
 # Private Subnets (for AKS Nodes/Pods)
@@ -20,7 +20,7 @@ resource "azurerm_subnet" "private_subnets" {
   name                 = "subnet-private-${count.index + 1}"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.${count.index + 3}.0/24"]
+  address_prefixes     = ["10.1.${count.index + 3}.0/24"]
   default_outbound_access_enabled = false
 }
 
