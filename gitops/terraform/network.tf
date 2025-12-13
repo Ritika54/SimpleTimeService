a2e587307f18dcc07d8686a30fed6088b00f6d47
Ritika Laddha <69119436+Ritika54@users.noreply.github.com>
2025-12-13 16:35:48 +0530

    adding NSG

--- gitops/terraform/network.tf
+++ gitops/terraform/network.tf
@@ -66,3 +66,54 @@ resource "azurerm_subnet_route_table_association" "private_subnet_associations" 
   subnet_id      = azurerm_subnet.private_subnets[count.index].id
   route_table_id = azurerm_route_table.aks_node_rt.id
 }
+
+resource "azurerm_network_security_group" "aks_nsg_private" {
+  name                = "aks-nsg-private"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  # Allow outbound internet access for updates/provisioning (handled by NAT GW)
+  security_rule {
+    name                       = "Allow_Outbound_Internet"
+    priority                   = 100
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_address_prefix      = "*"
+    source_port_range          = "*"
+    destination_address_prefix = "Internet"
+    destination_port_range     = "*"
+  }
+  
+  # Allow access to Azure services needed for provisioning (e.g., ACR, MCR)
+  security_rule {
+    name                       = "Allow_Outbound_AzureCloud"
+    priority                   = 110
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_address_prefix      = "*"
+    source_port_range          = "*"
+    destination_address_prefix = "AzureCloud" # Service Tag
+    destination_port_range     = "*"
+  }
+  
+  # Allow outbound access to Azure Container Registry (ACR) if needed later
+   security_rule {
+    name                       = "Allow_Outbound_ACR"
+    priority                   = 120
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "TCP"
+    source_address_prefix      = "*"
+    source_port_range          = "*"
+    destination_address_prefix = "AzureContainerRegistry" # Service Tag
+    destination_port_range     = "80,443"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "aks_nsg_private_association" {
+  count                     = 2
+  subnet_id                 = azurerm_subnet.private_subnets[count.index].id
+  network_security_group_id = azurerm_network_security_group.aks_nsg_private.id
+}
